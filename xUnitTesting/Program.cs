d59959f57442ad37a06e14b7a969066a5fbf25b7
goran jakovljevic <goran.jakovljevic@parallax-solutions.co.uk>
2018-06-03 23:07:50 +0100

    Add project files.

---
+++ xUnitTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xUnitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
